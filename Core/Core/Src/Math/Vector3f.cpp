3264e678ff414fbe502c13fffc24d0b866626c6e
Krajka <aleksandra.miska0@gmail.com>
2019-02-14 21:36:35 +0100

    I've made many mistakes

--- Core/Core/Src/Math/Vector3f.cpp
+++ Core/Core/Src/Math/Vector3f.cpp
@@ -100,8 +100,8 @@ Matrix3x3f core::math::Matrix3x3f::operator*(Vector3f const & v)
 	//Matrix3x3f **result;
 	for (int i = 0; i < 3; i++)
 	{
-		for (int j=0; j<3; j++)
-		result.Data[i][j]=
+		for (int j = 0; j < 3; j++)
+			result.Data[i][j] = Data[i][j] * v[i];
 	}
 	
 	return result;
